5cc7ae5fe5ec21557519e534b8b17d7f779f4544
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 06:27:32 +0700

    Add files via upload

---
+++ variable statis 3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int x;
+float func_1(int f1)
+{
+    int y1;
+}
+char y;
+int func_2()
+{
+    int x;
+}
+int func_3()
+{
+    {
+        int b;
+    }
+}
+int main()
+{
+    char y;
+    int x;
+}
